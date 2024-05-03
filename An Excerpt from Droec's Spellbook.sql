c69cd077b4a86fe4e5caeda2a429947887593aab
Cyberphysicist <168572819+Cyberphysicist@users.noreply.github.com>
2024-05-03 09:35:59 -0700

    Update An Excerpt from Droec's Spellbook.sql

--- An Excerpt from Droec's Spellbook.sql
+++ An Excerpt from Droec's Spellbook.sql
@@ -1,5 +1,5 @@
 /*
-Miller,Jonathan. "Exerpt from Droec's Spellbook," Telicom 35,no. 2 (July-September 2023):82.
+Miller,Jonathan. "An Excerpt from Droec's Spellbook," Telicom 35,no. 2 (July-September 2023):82.
 
 Poetic expression of a SQL query designed to visualize person to person connections in a network diagram.
 
